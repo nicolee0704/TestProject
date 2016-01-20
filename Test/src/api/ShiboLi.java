8f2bd39ddb67ea492b76f4dca451037742ce3a26
Nicholas Lee <nicolee0704@gmail.com>
2016-01-20 10:00:39 -0800

    Nicholas

--- Test/src/api/ShiboLi.java
+++ Test/src/api/ShiboLi.java
@@ -5,6 +5,8 @@ public class ShiboLi {
 	public static void main(String [] args) {
 		System.out.println("Hello, my name is Shbo Li");
 		System.out.println("This is another line");
+
+		System.out.println("This is the commit from nico");
 	}
 
 }
